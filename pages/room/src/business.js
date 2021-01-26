812b14a976238de189dce0772d73d5874e00dde0
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2021-01-26 17:29:37 -0400

    add Socket Web server with SocketIO and connection between client and server.

--- pages/room/src/business.js
+++ pages/room/src/business.js
@@ -1,9 +1,14 @@
 class Business {
-    constructor({ room, media, view }) {
+    constructor({ room, media, view, socketBuilder }) {
         this.room = room
         this.media = media
         this.view = view
 
+        this.socketBuilder = socketBuilder
+                            .setOnUserConnected(this.onUserConnected())
+                            .setOnUserDisconnected(this.onUserDisconnected())
+                            .build()
+        this.socketBuilder.emit('join-room', this.room, 'teste01')
         this.currentStream = {}
     }
 
@@ -23,5 +28,17 @@ class Business {
             stream,
             isCurrentId
         })
+    }
+
+    onUserconnected = function() {
+        return userId => {
+            console.log('user connected!', userId)
+        }
+    }
+
+    onUserDisconnected = function() {
+        return userId => {
+            console.log('user disconnected!', userId)
+        }
     }
 }
