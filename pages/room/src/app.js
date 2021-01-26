f2a92bab6fccf6752e2e79151af66b5f25a60874
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2021-01-26 09:28:50 -0400

    add view.js e util.js

---
+++ pages/room/src/app.js
@@ -0,0 +1,27 @@
+
+
+const recordClick = function (recorderBtn) {
+  this.recordingEnabled = false
+  return () => {
+    this.recordingEnabled = !this.recordingEnabled
+    recorderBtn.style.color = this.recordingEnabled ? 'red' : 'white'
+  }
+}
+
+const onload = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const room = urlParams.get('room');
+  console.log('this is the room', room)
+
+  //const recorderBtn = document.getElementById('record')
+  //recorderBtn.addEventListener('click', recordClick(recorderBtn))
+
+  const view = new View()
+  //view.renderVideo({ userId : 'test01', url : 'https://media.giphy.com/media/1pZ2kwvYvNMcX19x5z/giphy.mp4' })
+  //view.renderVideo({ userId : 'test01', isCurrentId: true, url : 'https://media.giphy.com/media/1pZ2kwvYvNMcX19x5z/giphy.mp4' })
+  //view.renderVideo({ userId : 'test02', url : 'https://media.giphy.com/media/1pZ2kwvYvNMcX19x5z/giphy.mp4' })
+
+
+}
+
+window.onload = onload
